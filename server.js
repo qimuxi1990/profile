43bbbb7e3aef6f8975f6acc1a1b2525abdd1b28d
Muxi Qi <qimuxi1990@users.noreply.github.com>
2016-09-26 13:22:36 -0400

    bugfix: use root instead of /static/

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var express = require('express');
 var app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, ip, function () {
   console.log('Example app listening on ' + ip + ':' + port + '!');
