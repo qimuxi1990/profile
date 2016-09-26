d0b42044806e4576237753351079abe9873103a2
Muxi Qi <qimuxi1990@users.noreply.github.com>
2016-09-26 11:24:33 -0400

    initial a static express server

---
+++ public/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('profileApp', ['ui.router', 'ngResource'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    // route for the home page
+    .state('app', {
+        url:'/',
+        views: {
+            'header': {
+                templateUrl : 'views/header.html'
+            },
+            'content': {
+                templateUrl : 'views/home.html',
+                //controller  : 'IndexController'
+            },
+            'footer': {
+                templateUrl : 'views/footer.html'
+            }
+        }
+    })
+    // route for the aboutus page
+    // .state('app.aboutus', {
+    //     url:'aboutus',
+    //     views: {
+    //         'content@': {
+    //             templateUrl : 'views/aboutus.html',
+    //             controller  : 'AboutController'                  
+    //         }
+    //     }
+    // })
+    $urlRouterProvider.otherwise('/');
+})
