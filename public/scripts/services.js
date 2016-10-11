4d8233c206431f202541c7e1250fef5d8e0c09a5
Muxi Qi <qimuxi1990@users.noreply.github.com>
2016-10-11 07:30:23 -0400

    user data updated

--- public/scripts/services.js
+++ public/scripts/services.js
@@ -37,7 +37,7 @@ angular.module('profileApp')
 		}], // required not empty
 		// Career Fields
 		/*adviceForContacting: '', // not required*/
-		summary: 'I am looking for full time jobs related to the industry of Computer Engineering', // required can empty
+		summary: 'I am an Electrical Engineering graduate student with also plenty of Computer Science and Data Science skills. My current objective is to look for a job in the industries related to Software or Computer Engineering.', // required can empty
 		position: 'Application Developer at Diamond Diagnostics', // required not empty
 		experience: [{
 			title: 'Engineering Intern',
@@ -52,15 +52,16 @@ angular.module('profileApp')
 			title: 'Application Developer',
 			description: '<u>Objective</u>: Perform Software/Program Development, Application Customization, Database/Reporting Development/Management, with tools like Mongo-Express-Angular-NodeJS, and SQL Server Reporting Services. Also complete any professional task or job function asked by the company.<br /><u>Content:</u> Full-Stack App Development based on Mongo-Express-Angular-NodeJS and ElasticSearch, Reporting Development based on SQL Server Reporting Services and also Visual Studio Tools for Office.',
 			company: 'Diamond Diagnostics Inc.',
-			address: '333 Fiske ST, Hollistion, MA 01701',
+			address: '333 Fiske ST, Hollistion, MA 01746',
 			logo: './images/logo-default.png',
 			industry: 'Medical Devices',
 			dateStart: new Date('2016-08-01'),
 			dateEnd: null
 		}], // required can empty
 		educations: [{
-			degree: 'Bachelor of Engineering',
-			major: 'Electrics Technology and Information Science',
+			degree: 'Bachelor of Engineering (B.E.)',
+			major: 'Electronics Information Science and Technology',
+			description: '2 years in Department of Physics. 3 years in Department of Electronic Engineering. <br />3 months research on Border Detection of ID Card Photos Taken by Mobile Phones as graduation project. <br />3 months assist researching on Visible Light Communication within Short Range in Optical Wireless Information System Lab (OWISYS).<br />3 months assist applying Compressed Sensing to Underwater Sensor in Research Institute of High-Speed Signal Processing And Network Transmission. <br />1 year plan, organize and promote students\' activities in class.',
 			organization: 'Tsinghua University',
 			logo: './images/logo-tsinghuauniversity.png',
 			department: 'Electronics Engineering',
@@ -68,30 +69,106 @@ angular.module('profileApp')
 			dateStart: new Date('2009-09-01'),
 			dateEnd: new Date('2014-07-14')
 		},{
-			degree: 'Master of Science',
-			major: 'Electrical & Computer Engineering',
+			degree: 'Master of Science (M.S.)',
+			major: 'Electrical and Electronics Engineering',
+			description: '2 years in Department of Electrical and Computer Engineering.<br />1 year research on extracted feature analysis for improving AlcoGait prediction accuracy as thesis project.',
 			organization: 'Worcester Polytechnic Institute',
 			logo: './images/logo-worcesterpolytechnicinstitute.png',
-			department: 'Electrical & Computer Engineering',
+			department: 'Electrical and Electronics Engineering',
 			gpa: 4.0,
 			dateStart: new Date('2014-08-28'),
 			dateEnd: new Date('2016-05-21')
 		}], // required can empty
 		skills: [{
 			name: 'Electrical Engineering',
-			endorsment: 3
+			endorsement: 3
 		},{
 			name: 'Computer Science',
-			endorsment: 3
-		},{
-			name: 'Signal Processing',
-			endorsment: 5
+			endorsement: 3
 		},{
 			name: 'Data Science',
-			endorsment: 1
+			endorsement: 1
+		},{
+			name: 'Signal Processing',
+			endorsement: 5
+		},{
+			name: 'Image Processing',
+			endorsement: 0
+		},{
+			name: 'Telecommunications',
+			endorsement: 0
+		},{
+			name: 'Network Engineering',
+			endorsement: 0
+		},{
+			name: 'Integrated Circuit Design',
+			endorsement: 0
+		},{
+			name: 'Computer Architecture',
+			endorsement: 0
+		},{
+			name: 'Programming',
+			endorsement: 3
 		},{
 			name: 'Databases',
-			endorsment: 1
+			endorsement: 1
+		},{
+			name: 'Web Development',
+			endorsement: 0
+		},{
+			name: 'C++',
+			endorsement: 3
+		},{
+			name: 'Java',
+			endorsement: 2
+		},{
+			name: 'Android',
+			endorsement: 1
+		},{
+			name: 'Verilog',
+			endorsement: 0
+		},{
+			name: 'VHDL',
+			endorsement: 0
+		},{
+			name: 'R',
+			endorsement: 0
+		},{
+			name: 'SQL',
+			endorsement: 0
+		},{
+			name: 'MongoDB',
+			endorsement: 0
+		},{
+			name: 'HTML5/CSS3',
+			endorsement: 0
+		},{
+			name: 'Node.js',
+			endorsement: 0
+		},{
+			name: 'JavaScript',
+			endorsement: 1
+		},{
+			name: 'ElasticSearch',
+			endorsement: 0
+		},{
+			name: 'SQL Server Reporting Services (SSRS)',
+			endorsement: 0
+		},{
+			name: 'Matlab',
+			endorsement: 1
+		},{
+			name: 'Altera Quartus',
+			endorsement: 0
+		},{
+			name: 'PSpice',
+			endorsement: 0
+		},{
+			name: 'Adobe Creative Suite',
+			endorsement: 0
+		},{
+			name: 'Microsoft Office',
+			endorsement: 0
 		}], // required
 		// Additional Career Fields
 		/*publications: [], // not required*/
