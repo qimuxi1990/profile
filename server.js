0e56d6763704205048fcc27dca5ce4f7aa0aa8c5
Muxi Qi <qimuxi1990@users.noreply.github.com>
2016-10-13 11:32:56 -0400

    RESTful NodeJS server enabled; Angular RESTful  service injected

--- server.js
+++ server.js
@@ -8,6 +8,36 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/', routes);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 app.listen(port, ip, function () {
   console.log('Example app listening on ' + ip + ':' + port + '!');
 });
